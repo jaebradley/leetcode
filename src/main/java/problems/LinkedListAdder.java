0c3965977dcf59ff973da27d39f5bd74afef37fd
jaebradley <jae.b.bradley@gmail.com>
2017-12-14 18:36:03 -0500

    fix typos

--- src/main/java/problems/LinkedListAdder.java
+++ src/main/java/problems/LinkedListAdder.java
@@ -4,10 +4,11 @@ import data.ListNode;
 
 /**
  *
- * https://leetcode.com/problems/two-sum/description/
+ * https://leetcode.com/problems/add-two-numbers/description/
  *
- * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list. You may assume the two numbers do not contain any leading zero, except the number 0 itself.
  */
 
 public class LinkedListAdder {
