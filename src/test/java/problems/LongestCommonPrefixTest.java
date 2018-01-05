dcc892a3508236fec0d05d6d79316edf2729e4ab
jaebradley <jae.b.bradley@gmail.com>
2018-01-04 23:38:59 -0500

    longest common prefix

---
+++ src/test/java/problems/LongestCommonPrefixTest.java
@@ -0,0 +1,41 @@
+package problems;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LongestCommonPrefixTest {
+    @Test
+    public void itShouldReturnPrefixesWhereFirstIsSubstring() {
+        String[] values = new String[] { "a", "ab", "abc" };
+        assertEquals("a", LongestCommonPrefix.longestCommonPrefix(values));
+    }
+
+    @Test
+    public void itShouldReturnPrefixesWhereSecondIsSubstringOfFirst() {
+        String[] values = new String[] { "aa", "a" };
+        assertEquals("a", LongestCommonPrefix.longestCommonPrefix(values));
+    }
+
+    @Test
+    public void itShouldReturnEmptyStringForNoCommonPrefix() {
+        String[] values = new String[] { "a", "b" };
+        assertEquals("", LongestCommonPrefix.longestCommonPrefix(values));
+    }
+
+    @Test
+    public void itShouldReturnEmptyStringForNull() {
+        assertEquals("", LongestCommonPrefix.longestCommonPrefix(null));
+    }
+
+    @Test
+    public void itShouldReturnEmptyStringForEmpty() {
+        assertEquals("", LongestCommonPrefix.longestCommonPrefix(new String[] {}));
+    }
+
+    @Test
+    public void itShouldReturnOnlyString() {
+        String[] values = new String[] { "jaebaebae" };
+        assertEquals("jaebaebae", LongestCommonPrefix.longestCommonPrefix(values));
+    }
+}
