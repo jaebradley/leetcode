bff9ca4dd84a72d0612774d4dc8a9e808d43e39d
jaebradley <jae.b.bradley@gmail.com>
2018-04-21 14:54:00 -0400

    number of islands

---
+++ src/test/java/problems/NumberOfIslandsTest.java
@@ -0,0 +1,37 @@
+package problems;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NumberOfIslandsTest {
+    @Test
+    public void zeroIslandsForGridWithOnlyZeros() {
+        char[][] grid = new char[][] {
+                new char[] {'0', '0', '0'},
+                new char[] {'0', '0', '0'},
+                new char[] {'0', '0', '0'},
+        };
+        assertEquals(0, NumberOfIslands.numIslands(grid));
+    }
+
+    @Test
+    public void zeroIslandForGridWithOne1() {
+        char[][] grid = new char[][] {
+                new char[] {'0', '0', '0'},
+                new char[] {'0', '1', '0'},
+                new char[] {'0', '0', '0'},
+        };
+        assertEquals(1, NumberOfIslands.numIslands(grid));
+    }
+
+    @Test
+    public void threeIslandForGridWithThreeUnconnectedPiecesOfLand() {
+        char[][] grid = new char[][] {
+                new char[] {'1', '0', '0'},
+                new char[] {'0', '1', '0'},
+                new char[] {'0', '0', '1'},
+        };
+        assertEquals(3, NumberOfIslands.numIslands(grid));
+    }
+}
