08311a32b157278f74687f629adeb59fe1dae5f4
Jae Bradley <jae.b.bradley@gmail.com>
2020-09-28 13:53:56 -0400

    Merge Two Binary Trees

---
+++ src/test/java/problems/MergeTwoBinaryTreesTest.java
@@ -0,0 +1,72 @@
+package problems;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MergeTwoBinaryTreesTest {
+    @Test
+    public void mergeComplete() {
+        TreeNode merged = new MergeTwoBinaryTrees().mergeTrees(
+                new TreeNode(
+                        1,
+                        new TreeNode(2),
+                        new TreeNode(3)
+                ),
+                new TreeNode(
+                        4,
+                        new TreeNode(5),
+                        new TreeNode(6)
+                )
+        );
+        assertEquals(merged.val, 5);
+        assertEquals(merged.left.val, 7);
+        assertEquals(merged.right.val, 9);
+    }
+
+    @Test
+    public void mergeIncompleteLeftChild() {
+        TreeNode merged = new MergeTwoBinaryTrees().mergeTrees(
+                new TreeNode(
+                        1,
+                        null,
+                        new TreeNode(3)
+                ),
+                new TreeNode(
+                        4,
+                        new TreeNode(5),
+                        new TreeNode(6)
+                )
+        );
+        assertEquals(merged.val, 5);
+        assertEquals(merged.left.val, 5);
+        assertEquals(merged.right.val, 9);
+    }
+
+    @Test
+    public void mergeIncompleteRightChild() {
+        TreeNode merged = new MergeTwoBinaryTrees().mergeTrees(
+                new TreeNode(
+                        1,
+                        new TreeNode(2),
+                        new TreeNode(3)
+                ),
+                new TreeNode(
+                        4,
+                        new TreeNode(5),
+                        null
+                )
+        );
+        assertEquals(merged.val, 5);
+        assertEquals(merged.left.val, 7);
+        assertEquals(merged.right.val, 3);
+    }
+
+    @Test
+    public void testMergeEmptyTrees() {
+        assertEquals(
+                null,
+                new MergeTwoBinaryTrees().mergeTrees(null, null)
+        );
+    }
+}
