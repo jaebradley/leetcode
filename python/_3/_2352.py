c3a4d5ecb4a3775925ee6cf50acfd5991d40c6f4
Jae Bradley <jae.b.bradley@gmail.com>
2023-08-07 10:43:42 -0400

    Add Trie Solution for Problem 2352

--- python/_3/_2352.py
+++ python/_3/_2352.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class HashSolution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         columns = [[0] * len(grid) for _ in range(len(grid[0]))]
         row_hashes = {}
@@ -18,5 +18,48 @@ class Solution:
             hashed_column = str(column)
             hashed_column_count = row_hashes.get(hashed_column, 0)
             pair_counter += hashed_column_count
+
+        return pair_counter
+
+
+class TrieSolution:
+    class Trie:
+        class Node:
+            def __init__(self, value: int):
+                self.value = value
+                self.count = 0
+                self.children = {}
+
+            def add_child(self, value: int):
+                child = self.children.get(value, TrieSolution.Trie.Node(value))
+                child.count += 1
+                self.children[value] = child
+                return child
+
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        head = TrieSolution.Trie.Node(None)
+        columns = [[0] * len(grid) for _ in range(len(grid[0]))]
+        for row_index, row in enumerate(grid):
+            current_node = head
+            for column_index, value in enumerate(row):
+                current_node = current_node.add_child(value)
+                columns[column_index][row_index] = value
+
+        def calculate_pairs(column: List[int]):
+            node = head.children.get(column[0])
+            if node is not None:
+                for v in column[1:]:
+                    node = node.children.get(v)
+                    if node is None:
+                        return 0
+
+                return node.count
+
+            return 0
+
+        pair_counter = 0
+
+        for column in columns:
+            pair_counter += calculate_pairs(column)
 
         return pair_counter
--- python/_3/test__2352.py
+++ python/_3/test__2352.py
@@ -1,14 +1,15 @@
 import pytest
 
-from _2352 import Solution
+from _2352 import HashSolution, TrieSolution
 
 
 class TestSolution:
     @pytest.mark.parametrize(
         "inputs, expected",
         [
-            ([[[3, 2, 1], [1, 7, 6], [2, 7, 7]]], 1),
+            # ([[[3, 2, 1], [1, 7, 6], [2, 7, 7]]], 1),
             ([[[3, 1, 2, 2], [1, 4, 4, 5], [2, 4, 2, 2], [2, 4, 2, 2]]], 3),
         ])
     def test(self, inputs, expected):
-        assert Solution().equalPairs(*inputs) == expected
+        for solution in [HashSolution(), TrieSolution()]:
+            assert solution.equalPairs(*inputs) == expected
