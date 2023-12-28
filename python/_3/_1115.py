d94113a8151a6d35368992903f39389d047ec672
Jae Bradley <jae.b.bradley@gmail.com>
2023-12-28 07:42:36 -0500

    Add other threading solutions for problem 1115

--- python/_3/_1115.py
+++ python/_3/_1115.py
@@ -1,4 +1,4 @@
-from threading import Lock, Barrier
+from threading import Lock, Barrier, Event, Semaphore
 
 
 class LockBasedFooBar:
@@ -40,3 +40,47 @@ class BarrierBasedFooBar:
         for i in range(self.n):
             self.barrier.wait()
             printBar()
+
+
+class EventBasedFooBar:
+    def __init__(self, n):
+        self.n = n
+        self.foo_printed_event = Event()
+        self.bar_printed_event = Event()
+
+        self.bar_printed_event.set()
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+
+        for i in range(self.n):
+            self.bar_printed_event.wait()
+            self.bar_printed_event.clear()
+            printFoo()
+            self.foo_printed_event.set()
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.foo_printed_event.wait()
+            self.foo_printed_event.clear()
+            printBar()
+            self.bar_printed_event.set()
+
+
+class SemaphoreBasedFooBar:
+    def __init__(self, n):
+        self.n = n
+        self.foo_semaphore = Semaphore(1)
+        self.bar_semaphore = Semaphore(0)
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+
+        for i in range(self.n):
+            self.foo_semaphore.acquire()
+            printFoo()
+            self.bar_semaphore.release()
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.bar_semaphore.acquire()
+            printBar()
+            self.foo_semaphore.release()
--- python/_3/test__1115.py
+++ python/_3/test__1115.py
@@ -27,3 +27,29 @@ class TestBarrierBasedFooBar:
 
         first_thread.start()
         second_thread.start()
+
+
+class TestEventBasedFooBar:
+    def test(self):
+        foobar = BarrierBasedFooBar(10)
+
+        first_thread = Thread(name="foo",
+                              target=lambda: foobar.foo(printFoo=lambda: print("foo")))
+        second_thread = Thread(name="bar",
+                               target=lambda: foobar.bar(printBar=lambda: print("bar")))
+
+        first_thread.start()
+        second_thread.start()
+
+
+class TestSemaphoreBasedFooBar:
+    def test(self):
+        foobar = BarrierBasedFooBar(10)
+
+        first_thread = Thread(name="foo",
+                              target=lambda: foobar.foo(printFoo=lambda: print("foo")))
+        second_thread = Thread(name="bar",
+                               target=lambda: foobar.bar(printBar=lambda: print("bar")))
+
+        first_thread.start()
+        second_thread.start()
