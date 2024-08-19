ae84ab2a426615447002a8e4950ff6311eff57ff
Jae Bradley <jae.b.bradley@gmail.com>
2024-08-18 22:10:48 -0400

    Problem 509

---
+++ python/_3/test_509.py
@@ -0,0 +1,14 @@
+import pytest
+
+from _509 import Solution
+
+class TestSolution:
+    @pytest.mark.parametrize(
+        "input, expected",
+        [
+            (0, 0),
+            (3, 2),
+            (4, 3),
+        ])
+    def test(self, input, expected):
+        assert expected == Solution().fib(input)
