43769bdc21e9f6d876c4a4d8dce29d36c18c1d4e
Jae Bradley <jae.b.bradley@gmail.com>
2024-08-22 00:54:24 -0400

    Problem 96 (#228)

---
+++ python/_3/test__96.py
@@ -0,0 +1,18 @@
+import pytest
+
+from _96 import TreeNode, Solution
+
+
+class TestSolution:
+    @pytest.mark.parametrize(
+        "node_count, expected",
+        [
+            (1, 1),
+            (2, 2),
+            (3, 5),
+            (4, 14),
+            (5, 42),
+            (6, 132),
+        ])
+    def test_level_order(self, node_count, expected):
+        assert Solution().numTrees(node_count) == expected
