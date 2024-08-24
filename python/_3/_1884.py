bcfc52a5d8a833c3f4d60c07de576d6c1c4095c5
Jae Bradley <jae.b.bradley@gmail.com>
2024-08-24 12:40:26 -0400

    Problem 1884 (#227)

---
+++ python/_3/_1884.py
@@ -0,0 +1,22 @@
+import sys
+
+class Solution:
+    def twoEggDrop(self, n: int) -> int:
+        result = [[0] * (1 + n), [0] * (1 + n), [0] * (1 + n)]
+
+        def drop(floors, eggs, result):
+            if eggs == 1 or floors <= 1:
+                return floors
+
+            if result[eggs][floors] > 0:
+                return result[eggs][floors]
+
+            current_minimum = sys.maxsize
+            for i in range(1, 1 + n):
+                current_minimum = min(current_minimum,
+                                      1 + max(drop(i - 1, eggs - 1, result), drop(floors - i, eggs, result)))
+
+            result[eggs][floors] = current_minimum
+            return current_minimum
+
+        return drop(n, 2, result)
---
+++ python/_3/test_1884.py
@@ -0,0 +1,13 @@
+import pytest
+
+from _1884 import Solution
+
+class TestSolution:
+    @pytest.mark.parametrize(
+        "input, expected",
+        [
+            (100, 14),
+            (2, 2),
+        ])
+    def test(self, input, expected):
+        assert expected == Solution().twoEggDrop(input)
