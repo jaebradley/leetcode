846fa61242f204c9f3c4d8a747bd7732ad740e3d
Jae Bradley <jae.b.bradley@gmail.com>
2024-10-04 16:40:27 -0400

    Problem 95 (#229)

---
+++ python/_3/test__95.py
@@ -0,0 +1,22 @@
+import pytest
+
+from _95 import TreeNode, Solution
+
+
+class TestSolution:
+    @pytest.mark.parametrize(
+        "node_count, expected",
+        [
+            (1, [TreeNode(val=1)]),
+            (2, [TreeNode(val=1, right=TreeNode(val=2)), TreeNode(val=2, left=TreeNode(val=1))]),
+            (3,
+             [
+                 TreeNode(val=1, right=TreeNode(val=2, right=TreeNode(val=3))),
+                 TreeNode(val=1, right=TreeNode(val=3, left=TreeNode(val=2))),
+                 TreeNode(val=2, left=TreeNode(val=1), right=TreeNode(val=3)),
+                 TreeNode(val=3, left=TreeNode(val=1, right=TreeNode(val=2))),
+                 TreeNode(val=3, left=TreeNode(val=2, left=TreeNode(val=1))),
+            ])
+        ])
+    def test_level_order(self, node_count, expected):
+        assert Solution().generateTrees(node_count) == expected
