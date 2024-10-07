86e999e239ea1bcb357626e15c14efc469bcadf2
Jae Bradley <jae.b.bradley@gmail.com>
2024-10-06 22:30:53 -0400

    Problem 373 (#223)

---
+++ python/_3/_373.py
@@ -0,0 +1,37 @@
+from heapq import heappop, heappush
+from typing import List
+
+
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        """
+        Idea is to first add all the pairs from all the values in nums1 and nums2[0]. The heap should now be ordered
+        with the minimum sums using only nums1 values and nums2[0].
+
+        Popping the first element of the heap should have the minimum sum, since it's using nums1[0] and nums2[0]. Add
+        this element to the pairs to return.
+
+        If the nums2 index value that was popped is less than the length of nums2 (i.e. there's a potential next nums2
+        value) add it to the heap as it could be a value that is less than all the current candidate nums1 + nums2[0]
+        sums.
+
+        Continue this k times to guarantee that at least k nums2 values have been explored.
+        """
+        pairs = []
+        heap = []
+        for i in range(len(nums1)):
+            heappush(heap, (nums1[i] + nums2[0], [i, 0]))
+
+        counter = k
+
+        while counter > 0 and heap:
+            value, (nums1_index, nums2_index) = heappop(heap)
+            pairs.append([nums1[nums1_index], nums2[nums2_index]])
+
+            if nums2_index < (len(nums2) - 1):
+                heappush(heap, (nums1[nums1_index] + nums2[1 + nums2_index], [nums1_index, 1 + nums2_index]))
+
+            counter -= 1
+
+
+        return pairs
---
+++ python/_3/test__373.py
@@ -0,0 +1,17 @@
+import pytest
+
+from _373 import Solution
+
+
+class TestSolution:
+    @pytest.mark.parametrize(
+        "values, expected",
+        [
+            ([[1, 7, 11], [2, 4, 6], 3], [[1,2],[1,4],[1,6]]),
+            ([[1, 1, 2], [1, 2, 3], 2], [[1,1],[1,1]]),
+            ([[1, 1, 1], [1, 1, 1], 2], [[1, 1], [1, 1]]),
+            ([[1, 1, 1], [1, 1, 1], 3], [[1, 1], [1, 1], [1, 1]]),
+            ([[1,2,4,5,6], [3,5,7,9], 3], [[1,3],[2,3],[1,5]]),
+        ])
+    def test(self, values, expected):
+        assert Solution().kSmallestPairs(*values) == expected
