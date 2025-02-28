1e7e59230942fc474c1ba77e53c8ea787c2d3bbd
Jae Bradley <jae.b.bradley@gmail.com>
2025-02-27 20:32:54 -0500

    Problem 572: Merkle tree solution (#241)

--- python/_3/_572.py
+++ python/_3/_572.py
@@ -1,3 +1,6 @@
+from hashlib import md5
+
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -9,7 +12,7 @@ class TreeNode:
 from typing import Optional
 
 
-class Solution:
+class RecursiveSolution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         if root is None:
             return False
@@ -27,3 +30,33 @@ class Solution:
         return current_root.val == current_comparison_root.val \
             and self.helper(current_root.left, current_comparison_root.left) \
             and self.helper(current_root.right, current_comparison_root.right)
+
+
+class MerkleTreeSolution:
+    """
+    hash of
+    * left node merkle value
+    * current node value
+    * right node merkle value
+    """
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        self.merkle(root)
+        self.merkle(subRoot)
+
+        def dfs(current_node: Optional[TreeNode]):
+            if not current_node:
+                return False
+            return current_node.merkle == subRoot.merkle or dfs(current_node.left) or dfs(current_node.right)
+
+        return dfs(root)
+
+    def merkle(self, node: Optional[TreeNode]):
+        if node:
+            left = self.merkle(node.left)
+            right = self.merkle(node.right)
+            hashed_value = md5(f"{left}{node.val}{right}".encode("ascii")).hexdigest()
+            node.merkle = hashed_value
+            return hashed_value
+
+        return "#"
