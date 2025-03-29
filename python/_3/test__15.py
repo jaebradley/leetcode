c5c631eb5951f1f8822089e180aa65149e9b4991
Jae Bradley <jae.b.bradley@gmail.com>
2025-03-28 21:39:20 -0400

    Two pointer three sum solution

--- python/_3/test__15.py
+++ python/_3/test__15.py
@@ -1,6 +1,6 @@
 import pytest
 
-from _15 import Solution
+from _15 import Solution, TwoPointerSolution
 
 
 class TestSolution:
@@ -10,6 +10,8 @@ class TestSolution:
             ([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
             ([0, 1, 1], []),
             ([0, 0, 0], [[0, 0, 0]]),
+            ([-2, 0, 1, 1, 2], [[-2, 0, 2], [-2, 1, 1]]),
         ])
     def test(self, input, expected):
-        assert sorted(Solution().threeSum(input)) == sorted(expected)
+        for solution in [Solution(), TwoPointerSolution()]:
+            assert sorted(solution.threeSum(input)) == sorted(expected)
