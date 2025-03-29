a8289c8232e7236efdc5f0df8e4df2b6d1b0bdd6
Jae Bradley <jae.b.bradley@gmail.com>
2025-03-29 16:32:22 -0400

    Two pointer three sum solution (#253)

--- python/_3/_15.py
+++ python/_3/_15.py
@@ -31,3 +31,38 @@ class Solution:
                     seen[right] = left_index
 
         return list(map(lambda v: list(v), results))
+
+
+class TwoPointerSolution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        sorted_nums = sorted(nums)
+        result = []
+        def find_triples(lowest_inclusive_index, highest_inclusive_index, target,  values, results):
+            while lowest_inclusive_index < highest_inclusive_index:
+                total = target + values[highest_inclusive_index] + values[lowest_inclusive_index]
+                if total < 0:
+                    lowest_inclusive_index += 1
+                elif total > 0:
+                    highest_inclusive_index -= 1
+                else:
+                    results.append([current_value, values[lowest_inclusive_index], values[highest_inclusive_index]])
+                    # decrement highest, because it's guaranteed (since the values are ordered) that the next
+                    # unique lower number will be greater than the current lower number, which will be a greater
+                    # sum than the current target
+                    highest_inclusive_index -= 1
+                    lowest_inclusive_index += 1
+                    while lowest_inclusive_index < highest_inclusive_index and values[lowest_inclusive_index] == values[lowest_inclusive_index - 1]:
+                        lowest_inclusive_index += 1
+
+        previous_value = None
+        for current_index in range(len(sorted_nums)):
+            current_value = sorted_nums[current_index]
+            if current_value > 0:
+                break
+
+            if current_value != previous_value:
+                find_triples(1 + current_index, len(nums) - 1, current_value, sorted_nums, result)
+
+            previous_value = current_value
+
+        return result
