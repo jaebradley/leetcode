5a366d6ee7c9f8d0d658599becab29189682f121
Jae Bradley <jae.b.bradley@gmail.com>
2025-05-05 22:26:49 -0400

    Problem 18

--- python/_3/_18.py
+++ python/_3/_18.py
@@ -2,55 +2,56 @@ from typing import List
 
 
 class Solution:
-    """
-    3 sum works by iterating over list, and then calling 2 sum for each element.
-    2 sum works by either iterating the lower or higher element depending on if the target sum is satisfied.
-    I think you want to call 3 sum for each element in this list
-    """
-
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        sorted_nums = sorted(nums)
-        previous_value, result = None, []
-        for current_index in range(len(sorted_nums)):
-            current_value = sorted_nums[current_index]
-            if current_value != previous_value:
-                for triplets in self.threeSum(sorted_nums=sorted_nums, target=target - current_value,
-                                              starting_index=1 + current_index):
-                    result.append([current_value] + triplets)
 
-            previous_value = current_value
+        def kSum(nums: List[int], target: int, k: int) -> List[List[int]]:
+            res = []
 
-        return result
+            # If we have run out of numbers to add, return res.
+            if not nums:
+                return res
 
-    def threeSum(self, sorted_nums, target, starting_index) -> List[List[int]]:
-        result, nums_count = [], len(sorted_nums)
+            # There are k remaining values to add to the sum. The
+            # average of these values is at least target // k.
+            average_value = target // k
 
-        def find_triples(lowest_inclusive_index, highest_inclusive_index, smallest_value, values, results):
-            while lowest_inclusive_index < highest_inclusive_index:
-                total = smallest_value + values[highest_inclusive_index] + values[lowest_inclusive_index]
-                if total < target:
-                    lowest_inclusive_index += 1
-                elif total > target:
-                    highest_inclusive_index -= 1
+            # We cannot obtain a sum of target if the smallest value
+            # in nums is greater than target // k or if the largest
+            # value in nums is smaller than target // k.
+            if average_value < nums[0] or nums[-1] < average_value:
+                return res
+
+            if k == 2:
+                return twoSum(nums, target)
+
+            for current_index in range(len(nums)):
+                # If current value is first number or if previous number is not equal to current number
+                if current_index == 0 or nums[current_index - 1] != nums[current_index]:
+                    # iterate over sums returned for decremented group size and decremented target value using remaining
+                    # numbers
+                    for subset in kSum(nums[current_index + 1:], target - nums[current_index], k - 1):
+                        res.append([nums[current_index]] + subset)
+
+            return res
+
+        def twoSum(nums: List[int], target: int) -> List[List[int]]:
+            res = []
+            lo, hi = 0, len(nums) - 1
+
+            while lo < hi:
+                curr_sum = nums[lo] + nums[hi]
+                if curr_sum < target or (lo > 0 and nums[lo] == nums[lo - 1]):
+                    lo += 1
+                elif curr_sum > target or (
+                        hi < len(nums) - 1 and nums[hi] == nums[hi + 1]
+                ):
+                    hi -= 1
                 else:
-                    results.append([smallest_value, values[lowest_inclusive_index], values[highest_inclusive_index]])
-                    # decrement highest, because it's guaranteed (since the values are ordered) that the next
-                    # unique lower number will be greater than the current lower number, which will be a greater
-                    # sum than the current target
-                    highest_inclusive_index -= 1
-                    lowest_inclusive_index += 1
-                    while lowest_inclusive_index < highest_inclusive_index and values[lowest_inclusive_index] == values[
-                        lowest_inclusive_index - 1]:
-                        lowest_inclusive_index += 1
+                    res.append([nums[lo], nums[hi]])
+                    lo += 1
+                    hi -= 1
 
-        previous_value = None if starting_index <= 0 else sorted_nums[starting_index - 1]
-        for current_index in range(starting_index, nums_count):
-            current_value = sorted_nums[current_index]
-            if current_value > target:
-                break
-            if current_value != previous_value:
-                find_triples(1 + current_index, nums_count - 1, current_value, sorted_nums, result)
+            return res
 
-            previous_value = current_value
-
-        return result
+        nums.sort()
+        return kSum(nums, target, 4)
