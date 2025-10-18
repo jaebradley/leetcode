d68e85d90043a95d593ee63b23822e429322f128
Jae Bradley <jae.b.bradley@gmail.com>
2025-10-18 12:54:58 -0400

    Problem 2493

---
+++ python/_3/test__2493.py
@@ -0,0 +1,15 @@
+import pytest
+
+from _2493 import Solution
+
+
+class TestSolution:
+    @pytest.mark.parametrize(
+        "inputs, expected",
+        [
+            ([6, [[1,2],[1,4],[1,5],[2,6],[2,3],[4,6]]], 4),
+            ([3, [[1,2],[2,3],[3,1]]], -1),
+            ([10, []], -1),
+        ])
+    def test(self, inputs, expected):
+        assert Solution().magnificentSets(*inputs) == expected
