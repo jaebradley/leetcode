d0f35530c8ae1c6fc7e113f5bc2bfd4a828a042b
Jae Bradley <jae.b.bradley@gmail.com>
2025-11-29 15:14:31 -0500

    Problem 621

--- python/_3/_621.py
+++ python/_3/_621.py
@@ -1,8 +1,9 @@
+import heapq
 from collections import Counter
 from typing import List
 
 
-class Solution:
+class MathematicalSolution:
     """
     Think of each grouping as array slots
     0: [a, b, c, etc, (and any "padding" before next "a/b/c")]
@@ -29,3 +30,35 @@ class Solution:
             most_common_elements += 1
 
         return max(len(tasks), (most_common_count - 1) * (n + 1) + most_common_elements)
+
+
+class PriorityQueueSolution:
+    """
+    Max heap of element with the greatest frequency element.
+    Pop elements off the heap and decrement their frequency.
+    Add these elements to a temporary array.
+    If heap is empty but n + 1 iterations haven't occurred yet, increment the counter by the remaining iterations.
+    Push elements onto the heap (or heapify the temporary array) and create a new temp array.
+    Repeat this process until heap is empty.
+
+    O(n) (for counting element frequency) + O(n x log k) (for heapifying the counted elements)
+    Since k = # of characters = 26, log 26 reduces to a constant.
+    O(n) + O(n x some constant) reduces to O(n)
+    """
+
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        time, c = 0, Counter(tasks)
+        character_counts = list(map(lambda v: -v[1], c.most_common()))
+        heapq.heapify(character_counts)
+        while character_counts:
+            seen_counts, remaining_iterations, task_count = [], n + 1, 0
+            while remaining_iterations > 0 and character_counts:
+                frequency = heapq.heappop(character_counts)
+                if frequency < -1:
+                    seen_counts.append(frequency + 1)
+                task_count += 1
+                remaining_iterations -= 1
+            for count in seen_counts:
+                heapq.heappush(character_counts, count)
+            time += n + 1 if character_counts else task_count
+        return time
--- python/_3/test__621.py
+++ python/_3/test__621.py
@@ -1,16 +1,18 @@
 import pytest
 
-from _621 import Solution
+from _621 import MathematicalSolution, PriorityQueueSolution
 
 
 class TestSolution:
     @pytest.mark.parametrize(
         "inputs, expected",
         [
-            ((["A","A","A","B","B","B"], 2), 8),
-            ((["A","C","A","B","D","B"], 1), 6),
-            ((["A","A","A", "B","B","B"], 3), 10),
+            ((["A", "A", "A", "B", "B", "B"], 2), 8),
+            ((["A", "C", "A", "B", "D", "B"], 1), 6),
+            ((["A", "A", "A", "B", "B", "B"], 3), 10),
+            ((["B", "C", "D", "A", "A", "A", "A", "G"], 1), 8),
         ]
     )
     def test(self, inputs, expected):
-        assert Solution().leastInterval(*inputs) == expected
+        for solution in (MathematicalSolution(), PriorityQueueSolution()):
+            assert solution.leastInterval(*inputs) == expected
