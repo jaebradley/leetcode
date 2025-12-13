5687094d213b1d07622526b9dfcc930a47350af8
Jae Bradley <jae.b.bradley@gmail.com>
2025-12-13 08:49:59 -0500

    Problem 2133

---
+++ python/_3/_2133.py
@@ -0,0 +1,39 @@
+from typing import List
+
+"""
+An n x n matrix is valid if every row and every column contains all the integers from 1 to n (inclusive).
+
+Given an n x n integer matrix matrix, return true if the matrix is valid. Otherwise, return false.
+
+Example 1:
+Input: matrix = [[1,2,3],[3,1,2],[2,3,1]]
+Output: true
+Explanation: In this case, n = 3, and every row and column contains the numbers 1, 2, and 3.
+Hence, we return true.
+
+Example 2:
+Input: matrix = [[1,1,1],[1,2,3],[1,2,3]]
+Output: false
+Explanation: In this case, n = 3, but the first row and the first column do not contain the numbers 2 or 3.
+Hence, we return false.
+ 
+
+Constraints:
+
+n == matrix.length == matrix[i].length
+1 <= n <= 100
+1 <= matrix[i][j] <= n
+"""
+
+class Solution:
+    def checkValid(self, matrix: List[List[int]]) -> bool:
+        c = set(range(1, len(matrix) + 1))
+        for row in matrix:
+            if c != set(row):
+                return False
+
+        for column_index in range(len(matrix)):
+            if c != set(map(lambda row: row[column_index], matrix)):
+                return False
+
+        return True
