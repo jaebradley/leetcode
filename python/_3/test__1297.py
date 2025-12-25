2eac42f0580767bec19272bf49437f832140dbdd
Jae Bradley <jae.b.bradley@gmail.com>
2025-12-25 11:46:47 -0500

    Problem 1297 (#390)

---
+++ python/_3/test__1297.py
@@ -0,0 +1,28 @@
+import pytest
+
+from _1297 import Solution
+
+
+class TestSolution:
+    @pytest.mark.parametrize(
+        "inputs, expected",
+        [
+            (
+                    ("aababcaab", 2, 3, 4),
+                    2
+            ),
+            (
+                    ("aaaa", 1, 3, 3),
+                    2
+            ),
+            (
+                    ("abcdaaaaaa", 1, 6, 6),
+                    1
+            ),
+            (
+                    ("abcde", 2, 3, 3),
+                    0
+            )
+        ])
+    def test(self, inputs, expected):
+        assert Solution().maxFreq(*inputs) == expected
